29d132158c215dcaff0d996c7a4bfd379e36196f
MarcinRapacz <marcin_04@onet.eu>
2020-11-04 19:18:51 +0100

    className fix

--- src/components/blog-list-home.js
+++ src/components/blog-list-home.js
@@ -6,11 +6,11 @@ import PostCard from "./post-card"
 
 const PostMaker = ({ data }) => (
   <section className="home-posts">
-    <h2>Ostatnio dodane <span class="icon -right"><RiArrowDownLine/></span></h2>
+    <h2>Ostatnio dodane <span className="icon -right"><RiArrowDownLine/></span></h2>
     <div className="grids col-1 sm-2 lg-3">
       {data}
     </div>
-    <Link className="button" to="/products">Zobacz więcej<span class="icon -right"><RiArrowRightSLine/></span></Link>
+    <Link className="button" to="/products">Zobacz więcej<span className="icon -right"><RiArrowRightSLine/></span></Link>
   </section>
 )
 
