f376e7382c00cafbdd5f20ccab8137ef63de3bba
MarcinRapacz <marcin_04@onet.eu>
2020-11-21 23:33:57 +0100

    update header size | fix site settings

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,8 @@
 import React from "react"
 
-const Header = ({children}) => (
+const Header = ({ children }) => (
   <header className="site-header">
-    <div className="site-header-content">
-      {children}
-    </div>
+    <div className="site-header-content">{children}</div>
   </header>
 )
 
