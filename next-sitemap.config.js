31a14169e259a8944a24de831ae29230568453d8
Marcin Rapacz <marcin_04@onet.eu>
2023-05-27 23:41:27 +0200

    update site map config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,37 @@
 module.exports = {
   siteUrl: process.env.SITE_URL,
   generateRobotsTxt: true,
+  transform: async (config, path) => {
+    if (path === "/favicon.ico") {
+      return null;
+    }
+
+    if (path === "/privacy-policy") {
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: 0.1,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      };
+    }
+
+    if (path === "/") {
+      return {
+        loc: path,
+        changefreq: config.changefreq,
+        priority: 0.9,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      };
+    }
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
